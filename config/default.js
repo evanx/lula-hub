df549dd1dae4dd1b90eed828166d77123bcbb633
Evan Summers <evan.summers@strongbyte.com>
2019-08-25 20:37:03 +0200

    Initial copy from fastify-redis-mlk

---
+++ config/default.js
@@ -0,0 +1,10 @@
+module.exports = {
+  port: 3000,
+  logger: {
+    level: 'info',
+  },
+  redis: {
+    host: '127.0.0.1',
+    keyPrefix: 'fr:',
+  },
+}
---
+++ config/development.js
@@ -0,0 +1,6 @@
+module.exports = {
+  logger: {
+    level: 'debug',
+    prettyPrint: { colorize: true, translateTime: true },
+  },
+}
